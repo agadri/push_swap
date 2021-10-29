e0c6f309607449f52289c59e965f0b4b22605df2
Adel GADRI <adegadri@e3r4p22.clusters.42paris.fr>
2021-10-29 19:11:15 +0200

    rectification

--- if_100.c
+++ if_100.c
@@ -1,11 +1,12 @@
-#include "push_swap.c"
+#include "push_swap.h"
 
-/*void    if_100(t_stack *stack)
+void    if_100(t_stack *stack)
 {
-	while (stack->len_s1 - 1 >= 0)
+	while ((stack->len_s1 - 1) >= 0)
 	{
-		put_int_place(stack);
 		smaller_to_the_top(stack);
-		pa(stack);
+		pb(stack);
 	}
-}*/
+	while ((stack->len_s2 - 1) >= 0)
+		pa(stack);
+}
